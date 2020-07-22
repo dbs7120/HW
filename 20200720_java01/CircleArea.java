d6d70cefdd2341931f698a0519c72a2b34303b06
윤예준 <dbm01049@gmail.com>
2020-07-22 06:05:58 +0000

    0720 원넓이

---
+++ 20200720_java01/CircleArea.java
@@ -0,0 +1,10 @@
+package com.java.first;
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+		double result = 0;
+		result = 5 * 5 * 3.14;
+		System.out.printf("반지름이 5Cm인 원의 넓이는 %.1fCm2입니다.",result);
+	}
+}
