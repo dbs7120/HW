71da24c35d3ad87508d9cfd57ef2b0439b163a39
Adj <dbs7120@naver.com>
2020-09-11 00:14:05 +0900

    자바스크립트 로또번호 생성기

---
+++ 20200910_FrontEnd03/main.js
@@ -0,0 +1,34 @@
+var count = 0; // 페이지내 번호 갯수 저장을위한 전역 변수
+
+function make() {
+  if (count != 6) {
+    let lotto = new Array(6);
+    let list = document.getElementById("list");
+
+    // call-back(recur)
+    let func = setInterval(function () {
+      let randNum = Math.floor(Math.random() * 45) + 1;
+      console.log(randNum);
+      for (let i = 0; i < count; i++) {
+        lotto[i] == randNum;
+      }
+      let list_item = document.createElement("li");
+      list_item.appendChild(document.createTextNode(randNum));
+      list.appendChild(list_item);
+      count++;
+      if (count >= 6) {
+        clearInterval(func);
+      }
+    }, 100);
+  }
+}
+function reset() {
+  var list = document.getElementById("list");
+  console.log(list);
+
+  // 다음자식값 존재 파악
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  }
+  count = 0;
+}
